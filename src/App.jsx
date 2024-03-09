9ce2965b75570f169f0809eff87bd240c2e7ef58
martinbanc <140523328+martinbanc@users.noreply.github.com>
2024-03-09 20:29:15 +0000

    added NavBar and style folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import './styles/main.css';
+
 function App() {
 	return (
-		<>
+		<div className="App">
+			<div className="NavBarContainer">  
+				<NavBar />
+			</div>
 			<h1>Hello Vite!</h1>
-		</>
+		</div>
 	);
 }
 
