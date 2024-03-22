e128ff312f7c491ff2dfc9c069d2937bc1a92a02
wumpfroot <markuskojo@msn.com>
2024-03-22 11:31:45 +0000

    Report embedded to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
-// import { NavLink } from "react-router-dom";
-import CardContainer from "../components/CardContainer";
-
-
 const Dashboard = () => {
 	return (
 		<div className="card">
-			<CardContainer />
+			<iframe
+				title="Industry Projects (PowerBI)"
+				width="1140"
+				height="541.25"
+				src="https://app.powerbi.com/reportEmbed?reportId=5b46ccb6-4ce2-4639-b88f-0fbf4412501b&autoAuth=true&ctid=a74c0ecf-ed67-4d73-aaa5-8ed7669b5780"
+				border="0"
+				allowFullScreen={true}
+			></iframe>
 		</div>
 	);
 };
